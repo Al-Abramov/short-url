b2e69cabe3380c981c6868bca1cc8d832efde380
Alex <x3m.f@mail.ru>
2022-09-03 13:48:54 +0400

    init: init project

---
+++ src/components/test/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+export const Test = () => {
+  return (
+    <div className={styles.TestContainer}>
+      <h1>Test</h1>
+    </div>
+  );
+};
